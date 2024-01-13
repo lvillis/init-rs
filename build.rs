6ba859c43e02833ffc04bba31fe641f523541650
lvillis <56720445+lvillis@users.noreply.github.com>
2024-01-13 12:14:17 +0800

    feat: install different software through features

--- build.rs
+++ build.rs
@@ -1,18 +1,34 @@
-use std::fs;
-use std::path::Path;
+use std::env;
+
 
 fn main() {
-    let (source_binary_path, target_path) = if cfg!(target_os = "windows") {
-        (Path::new("bin/just.exe"), Path::new("C:/Users/root/Documents/just.exe"))
-    } else if cfg!(target_os = "linux") {
-        (Path::new("bin/just"), Path::new("/root/just"))
-    } else {
-        panic!("Unsupported operating system");
-    };
+    #[cfg(target_os = "linux")]
+    {
+        let binary_name = if cfg!(feature = "just") {
+            "just"
+        } else if cfg!(feature = "fd") {
+            "fd"
+        } else {
+            panic!("No feature selected. Please build with --features just or --features fd.");
+        };
 
-    if let Err(e) = fs::copy(source_binary_path, &target_path) {
-        panic!("Failed to copy binary file: {}", e);
+        let source_binary_path = Path::new("bin").join(binary_name);
+        let target_path = Path::new("/root").join(binary_name);
+
+        if let Err(e) = fs::copy(&source_binary_path, &target_path) {
+            panic!("Failed to copy binary file '{}': {}", binary_name, e);
+        }
+
+        println!("Binary file '{}' copied to: {:?}", binary_name, target_path);
     }
 
-    println!("Binary file copied to: {:?}", target_path);
+    #[cfg(not(target_os = "linux"))]
+    {
+        if cfg!(debug_assertions) {
+            println!("Running in local development (debug) mode on non-Linux OS...");
+        } else {
+            //panic!("This package is specifically designed for Linux OS.");
+            panic!("Non-Linux OS detected: {}", env::consts::OS);
+        }
+    }
 }
