574f288384ab9631c0aff2eb9255993c614eda60
lvillis <56720445+lvillis@users.noreply.github.com>
2024-01-13 10:52:48 +0800

    chore: add more info

--- build.rs
+++ build.rs
@@ -7,11 +7,9 @@ fn main() {
     } else if cfg!(target_os = "linux") {
         (Path::new("bin/just"), Path::new("/root/just"))
     } else {
-        // 如果操作系统既不是 Windows 也不是 Linux，退出构建脚本
         panic!("Unsupported operating system");
     };
 
-    // 复制二进制文件
     if let Err(e) = fs::copy(source_binary_path, &target_path) {
         panic!("Failed to copy binary file: {}", e);
     }
